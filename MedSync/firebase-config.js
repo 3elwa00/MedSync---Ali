726c46209f30d3f29e7b3a49d17dd188ad046e28
3elwa00 <114284893+3elwa00@users.noreply.github.com>
2025-06-13 10:30:28 +0300

    Update firebase-config.js

--- MedSync/firebase-config.js
+++ MedSync/firebase-config.js
@@ -14,3 +14,7 @@ firebase.initializeApp(firebaseConfig);
 
 // Make Firestore available globally
 window.db = firebase.firestore();
+
+import { getFirestore } from "firebase/firestore";
+const db = getFirestore(app);
+export { db };
